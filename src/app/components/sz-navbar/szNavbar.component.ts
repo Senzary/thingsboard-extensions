28c38cbd28d7baf0d6cd15ce5182de49b696be6e
Ernesto Medina <ernesto.m.medina.m@gmail.com>
2025-02-01 12:35:27 -0400

    :construction: Builds dummy responsive navbar loading user & scope

--- src/app/components/sz-navbar/szNavbar.component.ts
+++ src/app/components/sz-navbar/szNavbar.component.ts
@@ -1,17 +1,39 @@
-import { Component } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 import { Store } from "@ngrx/store";
 import { AppState } from "@core/core.state";
+import { BreakpointObserver, BreakpointState } from "@angular/cdk/layout";
+import { Observable, of } from "rxjs";
+import { OrganisationUser } from "../../sz-models/public-api";
+import { UserService } from "@core/public-api";
+import { WidgetContext } from "@modules/home/models/widget-component.models";
+import { UserInfo } from "@shared/public-api";
 
 @Component({
     selector: 'tb-sz-navbar',
     templateUrl: './szNavbar.component.html',
     styleUrls: ['./szNavbar.component.scss']
 }) 
-export class SZNavbarComponent {
+export class SZNavbarComponent implements OnInit {
+    @Input() ctx: WidgetContext;
     store: Store<AppState>;
+    notMobile$: Observable<BreakpointState>;
+    userService: UserService;
+    user$: Observable<UserInfo>;
+    userManager: OrganisationUser;
     constructor(
-        store: Store<AppState>
+        store: Store<AppState>,
+        private breakpointObserver: BreakpointObserver,
+        userService: UserService
     ) {
-        this.store = store;
+        this.userService = userService;
+        this.notMobile$ = this.breakpointObserver.observe([
+            "xl",
+            "lg",
+            "md"
+        ]);
+    };
+    ngOnInit(): void {
+        this.userManager = new OrganisationUser(this.userService);
+        this.user$ = this.userManager.loadFromAuthUser(this.ctx.currentUser);
     };
 };
--- src/app/components/sz-navbar/szNavbar.module.ts
+++ src/app/components/sz-navbar/szNavbar.module.ts
@@ -3,10 +3,12 @@ import { SZNavbarComponent } from "./szNavbar.component";
 import { IconModule } from "../../modules/public-api";
 import { CommonModule } from "@angular/common";
 import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatIconModule } from "@angular/material/icon";
 
 @NgModule({
     declarations: [SZNavbarComponent],
     imports: [
+        MatIconModule,
         IconModule,
         CommonModule,
         MatToolbarModule
