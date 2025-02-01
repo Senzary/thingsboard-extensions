c66d706ef9042bb0a36d72de4f6e0a9b6bff99ee
Ernesto Medina <ernesto.m.medina.m@gmail.com>
2025-02-01 12:35:27 -0400

    :construction: Builds dummy responsive navbar loading user & scope

--- src/app/modules/icon.module.ts
+++ src/app/modules/icon.module.ts
@@ -6,14 +6,14 @@ import { DomSanitizer, SafeResourceUrl } from "@angular/platform-browser";
     imports: [MatIconModule]
 })
 export class IconModule {
-    private path: string = "../../assets"
+    private path: string = "/api/images/public";
     constructor(
         private domSanitizer: DomSanitizer,
         public matIconRegistry: MatIconRegistry
     ) {
         this.matIconRegistry.addSvgIcon(
             "senzary-isotype", 
-            this.setPath(`${this.path}/senzary-isotype.svg`));
+            this.setPath(`${this.path}/W1X6EmcMXhgVZ0EYj0PGPtCQ0jxEM4SW`));
     };
     private setPath(url: string): SafeResourceUrl {
         return this.domSanitizer.bypassSecurityTrustResourceUrl(url);
